a1a6ac72b1d04d422a8d0c440bc8ca97a5b24ca6
Alex <alexargdev@gmail.com>
2025-01-09 16:22:43 -0300

    Test signer passed with data, execution time rests.

--- internal/week2/hw/signer.go
+++ internal/week2/hw/signer.go
@@ -2,12 +2,48 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strconv"
+	"strings"
 	"sync"
 )
 
 func SingleHash(in, out chan interface{}) {
-	fmt.Printf("SingleHash reads value %#v\n", <-out)
+	for rawData := range in {
+		data, ok := rawData.(int)
+		if !ok {
+			panic(fmt.Sprintf("type of the input value (%#v) is not int", rawData))
+		}
+		// crc32(data)+"~"+crc32(md5(data))
+		out <- DataSignerCrc32(strconv.Itoa(data)) + "~" + DataSignerCrc32(DataSignerMd5(strconv.Itoa(data)))
+	}
+}
 
+func MultiHash(in, out chan interface{}) {
+	for rawData := range in {
+		data, ok := rawData.(string)
+		if !ok {
+			panic(fmt.Sprintf("type of the input value (%#v) is not string", rawData))
+		}
+		results := ""
+		for i := 0; i < 6; i++ {
+			results += DataSignerCrc32(strconv.Itoa(i) + data)
+		}
+		out <- results
+	}
+}
+
+func CombineResults(in, out chan interface{}) {
+	results := []string{}
+	for v := range in {
+		s, ok := v.(string)
+		if !ok {
+			panic(fmt.Sprintf("type of the input value (%#v) is not int", v))
+		}
+		results = append(results, s)
+	}
+	sort.Strings(results)
+	out <- strings.Join(results, "_")
 }
 
 func ExecutePipeline(hashSignJobs ...job) {
