1736d583b8db728d4f6dd79b846f176adfd9501b
Alex <alexargdev@gmail.com>
2025-01-09 12:02:04 -0300

    Test by Ilya passed.

--- internal/week2/hw/signer.go
+++ internal/week2/hw/signer.go
@@ -1,3 +1,45 @@
 package main
 
-// сюда писать код
+import (
+	"fmt"
+	"sync"
+)
+
+func SingleHash(in, out chan interface{}) {
+	fmt.Printf("SingleHash reads value %#v\n", <-out)
+
+}
+
+func ExecutePipeline(hashSignJobs ...job) {
+
+	defer fmt.Printf("STOP of ExecutePipeline\n")
+
+	var in, out chan interface{}
+
+	wg := &sync.WaitGroup{}
+
+	in = make(chan interface{})
+
+	out = make(chan interface{})
+	wg.Add(1)
+	go func(in, out chan interface{}) {
+		defer wg.Done()
+		hashSignJobs[0](in, out)
+	}(in, out)
+	in = out
+	out = make(chan interface{})
+	go func(in, out chan interface{}) {
+		defer wg.Done()
+		hashSignJobs[1](in, out)
+	}(in, out)
+	in = out
+	out = make(chan interface{})
+	go func(in, out chan interface{}) {
+		defer wg.Done()
+		hashSignJobs[2](in, out)
+	}(in, out)
+
+	fmt.Printf("TEST EXECUTING\n")
+
+	wg.Wait()
+}
