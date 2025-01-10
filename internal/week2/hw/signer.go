2156abcc8d829ef747556005b6d7604442fc9ae6
Alex <alexargdev@gmail.com>
2025-01-10 10:40:56 -0300

    hw(week2): finished

--- internal/week2/hw/signer.go
+++ internal/week2/hw/signer.go
@@ -8,9 +8,12 @@ import (
 	"sync"
 )
 
+// возвращает строку вида
+// crc32(data)+"~"+crc32(md5(data))
 func SingleHash(in, out chan interface{}) {
 	// канал квоты для функции DataSignerMd5, чтобы избежать перегрева
-	quotaCh := make(chan struct{}, 1)
+	// т.к. функция DataSignerMd5 не может быть распараллелена
+	md5QuotaCh := make(chan struct{}, 1)
 
 	wg := &sync.WaitGroup{}
 	for rawData := range in {
@@ -21,34 +24,62 @@ func SingleHash(in, out chan interface{}) {
 		wg.Add(1)
 		go func(out chan interface{}) {
 			defer wg.Done()
-			// crc32(data)+"~"+crc32(md5(data))
-			quotaCh <- struct{}{}
+			// md5
+			// забираем квоту, записывая токен в канал
+			md5QuotaCh <- struct{}{}
+			// считаем хеш
 			md5 := DataSignerMd5(strconv.Itoa(data))
-			<- quotaCh
-			out <- DataSignerCrc32(strconv.Itoa(data)) + "~" + DataSignerCrc32(md5)
+			// читаем наш токен, возвращая квоту для других горутин
+			<-md5QuotaCh
+
+			// массив для результатов распараллеливания crc32 функции
+			crc := [2]string{}
+			wg2 := &sync.WaitGroup{}
+			wg2.Add(1)
+			go func(data int) {
+				defer wg2.Done()
+				crc[0] = DataSignerCrc32(strconv.Itoa(data))
+			}(data)
+			wg2.Add(1)
+			go func(data int) {
+				defer wg2.Done()
+				crc[1] = DataSignerCrc32(md5)
+			}(data)
+			wg2.Wait()
+
+			out <- crc[0] + "~" + crc[1]
 		}(out)
 	}
 	wg.Wait()
 }
 
 func MultiHash(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
 	for rawData := range in {
 		data, ok := rawData.(string)
 		if !ok {
 			panic(fmt.Sprintf("type of the input value (%#v) is not string", rawData))
 		}
-		wg := &sync.WaitGroup{}
-		results := make([]string, 6)
-		for i := 0; i < 6; i++ {
-			wg.Add(1)
-			go func(i int, data string) {
-				defer wg.Done()
-				results[i] = DataSignerCrc32(strconv.Itoa(i) + data)
-			}(i, data)
-		}
-		wg.Wait()
-		out <- strings.Join(results, "")
+		// поступившие в канал ззначения распараллеливаем сразу
+		wg.Add(1)
+		go func(out chan interface{}) {
+			defer wg.Done()
+			wg2 := &sync.WaitGroup{}
+			// и внутри job распараллеливаем подсчет crc32
+			// записываем в массив, каждая ячейка для одной горутины
+			results := [6]string{}
+			for i := 0; i < 6; i++ {
+				wg2.Add(1)
+				go func(i int, data string) {
+					defer wg2.Done()
+					results[i] = DataSignerCrc32(strconv.Itoa(i) + data)
+				}(i, data)
+			}
+			wg2.Wait()
+			out <- strings.Join(results[:], "")
+		}(out)
 	}
+	wg.Wait()
 }
 
 func CombineResults(in, out chan interface{}) {
@@ -72,6 +103,7 @@ func ExecutePipeline(hashSignJobs ...job) {
 
 	for i, j := range hashSignJobs {
 		wg.Add(1)
+		// по горутине на каждую job
 		go func(in, out chan interface{}, j job, i int) {
 			defer wg.Done()
 			j(in, out)
